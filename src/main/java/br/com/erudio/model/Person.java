3aaa9b7f3ccd3d040dbbfcd79e7bfbfa90daf8fe
JefersonT <jefersontorres.f@gmail.com>
2023-05-27 10:55:19 -0300

    Cria Verbos HTTP - GET, POST, PUT e DELETE

---
+++ src/main/java/br/com/erudio/model/Person.java
@@ -0,0 +1,19 @@
+package br.com.erudio.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String gender;
+}
