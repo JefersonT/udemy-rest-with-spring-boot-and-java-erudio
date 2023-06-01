744497791305b0e8bc622d8585a8d4b737e8d9ce
JefersonT <jefersontorres.f@gmail.com>
2023-05-31 21:14:05 -0300

    Seção12 - Cusotm JSON Serialization

--- src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
+++ src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
@@ -1,5 +1,8 @@
 package br.com.erudio.data.vo.v1;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,14 +10,21 @@ import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
+@JsonPropertyOrder({"id", "gender", "first_Name", "last_Name", "address"})
 public class PersonVO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
 
+
     private Long id;
+    @JsonProperty("first_Name")
     private String firstName;
+
+    @JsonProperty("last_Name")
     private String lastName;
     private String address;
+
+    @JsonIgnore
     private String gender;
 }
