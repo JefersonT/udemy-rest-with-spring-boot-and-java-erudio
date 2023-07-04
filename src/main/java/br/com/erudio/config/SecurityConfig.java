39cd84b0ef443e0c02ca7e875ea9f4f82448a0b2
JefersonT <jefersontorres.f@gmail.com>
2023-07-04 17:35:52 -0300

    Seção17 - Ajustes de Deprecated

--- src/main/java/br/com/erudio/config/SecurityConfig.java
+++ src/main/java/br/com/erudio/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -44,25 +45,24 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .httpBasic().disable()
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(
-                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(
-                        authorizeHttpRequests -> authorizeHttpRequests
-                                .requestMatchers(
-                                        "/auth/signin",
-                                        "/auth/refresh/**",
-                                        "/v3/api-docs/**",
-                                        "/swagger-ui/**").permitAll()
-                                .requestMatchers("/api/**").authenticated()
-                                .requestMatchers("/users").denyAll()
-                )
-                .cors()
-                .and()
-                .apply(new JwtConfigurer(tokenProvider))
-                .and()
-                .build();
+        http
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(
+                    session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(
+                    authorizeHttpRequests -> authorizeHttpRequests
+                            .requestMatchers(
+                                    "/auth/signin",
+                                    "/auth/refresh/**",
+                                    "/v3/api-docs/**",
+                                    "/swagger-ui/**").permitAll()
+                            .requestMatchers("/api/**").authenticated()
+                            .requestMatchers("/users").denyAll()
+            )
+            .cors(Customizer.withDefaults())
+            .apply(new JwtConfigurer(tokenProvider));
+
+        return http.build();
     }
 }
