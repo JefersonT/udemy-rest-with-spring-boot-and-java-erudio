a87bc3e07fcd77a54f9e80a3e60671ec9dac3df9
JefersonT <tqi_jtorres@uolinc.com>
2023-08-10 20:50:58 -0300

    Seção20.15 - Ajustando os Testes de HATEOAS quebrados no Endpoint de Person

--- src/test/java/br/com/erudio/unittests/mockito/services/PersonServicesTest.java
+++ src/test/java/br/com/erudio/unittests/mockito/services/PersonServicesTest.java
@@ -58,57 +58,6 @@ class PersonServicesTest {
 
     }
 
-//    @Test
-//    void testFindAll() {
-//        List<Person> list = input.mockEntityList();
-//
-//        when(personRepository.findAll()).thenReturn(list);
-//
-//        var people = personServices.findAll(pageable);
-//
-//        assertNotNull(people);
-//        assertEquals(14, people.size());
-//
-//        var personOne = people.get(1);
-//
-//        assertNotNull(personOne);
-//        assertNotNull(personOne.getKey());
-//        assertNotNull(personOne.getLinks());
-//
-//        assertTrue(personOne.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
-//
-//        assertEquals("Addres Test1", personOne.getAddress());
-//        assertEquals("Female", personOne.getGender());
-//        assertEquals("First Name Test1", personOne.getFirstName());
-//        assertEquals("Last Name Test1", personOne.getLastName());
-//
-//        var personFour = people.get(4);
-//
-//        assertNotNull(personFour);
-//        assertNotNull(personFour.getKey());
-//        assertNotNull(personFour.getLinks());
-//
-//        assertTrue(personFour.toString().contains("links: [</api/person/v1/4>;rel=\"self\"]"));
-//
-//        assertEquals("Addres Test4", personFour.getAddress());
-//        assertEquals("Male", personFour.getGender());
-//        assertEquals("First Name Test4", personFour.getFirstName());
-//        assertEquals("Last Name Test4", personFour.getLastName());
-//
-//        var personSeven = people.get(7);
-//
-//        assertNotNull(personSeven);
-//        assertNotNull(personSeven.getKey());
-//        assertNotNull(personSeven.getLinks());
-//
-//        assertTrue(personSeven.toString().contains("links: [</api/person/v1/7>;rel=\"self\"]"));
-//
-//        assertEquals("Addres Test7", personSeven.getAddress());
-//        assertEquals("Female", personSeven.getGender());
-//        assertEquals("First Name Test7", personSeven.getFirstName());
-//        assertEquals("Last Name Test7", personSeven.getLastName());
-//    }
-
     @Test
     void testCreate() {
         Person entity = input.mockEntity(1);
